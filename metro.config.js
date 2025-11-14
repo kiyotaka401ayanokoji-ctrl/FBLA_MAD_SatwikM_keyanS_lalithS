4efd9277acc658bbe2736a475e490f26d8a8635f
AyanokojiKiyotaka007 <unsimplifiedradical@gmail.com>
2025-11-13 20:34:05 -0800

    Update metro.config.js redooo

--- metro.config.js
+++ metro.config.js
@@ -1,21 +1,9 @@
-const { wrapWithReanimatedMetroConfig } = require('react-native-reanimated/metro-config');
+const {
+  wrapWithReanimatedMetroConfig,
+} = require('react-native-reanimated/metro-config');
 const { getDefaultConfig } = require('@expo/metro-config');
 
+/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
-
-// Correct custom resolver
-config.resolver.resolveRequest = (context, moduleName, platform) => {
-  // Intercept expo-speech on web and return an empty JS module
-  if (platform === 'web' && moduleName === 'expo-speech') {
-    return {
-      type: 'source',
-      filePath: moduleName,
-      src: 'export default {};'
-    };
-  }
-
-  // Use default resolver for everything else
-  return context.resolveRequest(context, moduleName, platform);
-};
 
 module.exports = wrapWithReanimatedMetroConfig(config);
